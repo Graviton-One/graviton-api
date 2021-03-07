ad6fa2bc914d7cdf868ba6a40100232c9391bc0a
sapranidi <sapranidi@users.noreply.github.com>
2021-03-08 00:43:10 +0300

    Create initial deployment setup

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+git clean -fd
+git reset --hard
+# branch=$(git branch | grep '*' | awk '{ print $2 }')
+git_c () {
+  ssh-agent bash -c "ssh-add /root/keys_gh/gh; $1"
+}
+# git_c "git pull origin $branch"
+git_c "git pull origin develop"
+git_c "git checkout -b develop"
+
+echo Installing requirements via pip...
+pip3 install -r requirements.txt
+
+echo Creating a virtual environment...
+virtualenv -p ./venv
+
+echo Activating the virtual environment...
+source ./venv/bin/activate
+
+echo Launching the API server...
+python3 api.py
